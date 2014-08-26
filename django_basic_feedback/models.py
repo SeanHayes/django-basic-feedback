a8411473c12e47506cfd4bc4eaf650e018d240df
Luan <luanpab@gmail.com>
2014-08-26 13:04:48 -0300

    pointing to AUTH_USER_MODEL

--- django_basic_feedback/models.py
+++ django_basic_feedback/models.py
@@ -5,11 +5,11 @@
 from datetime import datetime
 
 #Django imports
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 
 class Feedback(models.Model):
-	user = models.ForeignKey(User)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL)
 	date = models.DateTimeField(auto_now_add=True)
 	page = models.CharField(max_length="255", blank=True)
 	text = models.TextField()
